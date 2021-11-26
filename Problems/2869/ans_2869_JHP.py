41b6e95da86b5c6620277c506940c34dcafef221
Bobzzang <jaehyun_22@naver.com>
2021-11-26 16:28:30 +0900

    Daily commit-1 for 2021.11.26

---
+++ Problems/2869/ans_2869_JHP.py
@@ -0,0 +1,30 @@
+# https://www.acmicpc.net/problem/2775
+
+# 문제 : V미터인 나무 오르는데 걸리는 시간 출력
+# 입력 : 낮 = A미터, 밤 = -B미터, 나무 길이 = V
+
+def solve():
+    A,B,V = list(map(int,input().split()))
+
+    temp = 0
+    day_mov = 1
+
+    while temp < V:
+        
+        temp = (A-B)*day_mov
+        
+        if V-temp > A:
+            day_mov += 1
+        else:
+            day_mov += 1
+            break
+
+    print(day_mov)
+
+
+if __name__ == "__main__":
+    solve()
+
+# 2 1 5 -> 4
+# 5 1 6 -> 2
+# 100 99 1000000000 -> 999999901
