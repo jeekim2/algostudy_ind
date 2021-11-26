9a486f97e896dfb8cc62ef1cc853d426c2336c83
hongmyeong-lee <dlghdaud16@naver.com>
2021-11-26 22:36:06 +0900

    remove가 왜 안되지?

---
+++ Problems/1002/1002.py
@@ -0,0 +1,42 @@
+#https://www.acmicpc.net/problem/1002
+
+#만나는 점 0개
+#(x2-x1)^2+(y2-y1)^2 의 루트 값  >  r1+r2
+#만나는 점 1개
+#(x2-x1)^2+(y2-y1)^2 의 루트 값  =  r1+r2
+#만나는 점 2개
+#(x2-x1)^2+(y2-y1)^2 의 루트 값  <  r1+r2
+a = [1,2,3]
+b = a.remove(1)
+print(b[0])
+
+T = int(input())
+a =[]
+b =[]
+
+'''
+for i in range(T):
+    a = input()
+    b = a.split()
+    if i == T:
+        break
+    if (int(b[3])==int(b[0]))and (int(b[4])==int(b[1])):
+        if int(b[2])==int(b[5]):
+            print(-1)
+        else:
+            print(0)
+    else:
+        a0 = {((int(b[3])-int(b[0]))**2 + (int(b[4])-int(b[1]))**2)**0.5, int(b[2]),int(b[5])}
+        a1 = max(((int(b[3])-int(b[0]))**2 + (int(b[4])-int(b[1]))**2)**0.5, int(b[2]),int(b[5]))
+        a2 = a0.remove(a1)
+
+        if int(a1) > int(sum((a2))):
+                print(0)
+
+        if int(a1) == int(sum((a2))):
+                print(1)
+
+        if int(a1) < int(sum((a2))):
+                print(2)
+
+'''
