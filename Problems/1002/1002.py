295b8caaf8219770618690e120bf3d641fbe6eec
hongmyeong-lee <dlghdaud16@naver.com>
2021-11-27 14:35:22 +0900

    왜 pass 못하는지 모르겠음

--- Problems/1002/1002.py
+++ Problems/1002/1002.py
@@ -8,30 +8,26 @@
 #(x2-x1)^2+(y2-y1)^2 의 루트 값  <  r1+r2
 
 T = int(input())
-a =[]
-b =[]
+#a =[]
+#b =[]
 
 for i in range(T):
-    a = input()
-    b = a.split()
-    if i == T:
-        break
-    if (int(b[3])==int(b[0]))and (int(b[4])==int(b[1])):
-        if int(b[2])==int(b[5]):
+    #a = input()
+    #b = a.split()
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    r = ((x1-y1)**2 + (x2-y2)**2)**0.5
+    R = [r1,r2,r]
+    M = max(R)
+    R.remove(M)
+    if r == 0:
+        if r1 == r2:
             print(-1)
         else:
             print(0)
     else:
-        a0 = {((int(b[3])-int(b[0]))**2 + (int(b[4])-int(b[1]))**2)**0.5, int(b[2]),int(b[5])}
-        a1 = max(((int(b[3])-int(b[0]))**2 + (int(b[4])-int(b[1]))**2)**0.5, int(b[2]),int(b[5]))
-        a2 = a0.remove(a1)
-
-        if int(a1) > int(sum((a0))):
-            print(0)
-
-        elif int(a1) == int(sum((a0))):
+        if M == sum(R):
             print(1)
-
+        elif M > sum(R):
+            print(0)
         else:
             print(2)
-
