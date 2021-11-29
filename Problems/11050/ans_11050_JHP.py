c5d291697799fc079a70078fdc3840af9a9c3cc6
Bobzzang <jaehyun_22@naver.com>
2021-11-29 22:27:34 +0900

    Daily Commit on 2021.11.29 : Solve 11050 with recursive function call

--- Problems/11050/ans_11050_JHP.py
+++ Problems/11050/ans_11050_JHP.py
@@ -6,49 +6,20 @@
 # 아래 For를 이용하면 Run time Error 발생
 # 따라서, 재귀함수의 활용하는 것으로 Runtime Error를 방지할 필요가 있음. (재귀로 다시 풀어보자)
 
+import sys
+
+def recursive(num):
+    if num <= 1: # num-1이 0,1이 되었을 시 처리 필요
+        return 1
+    else:
+        return num*recursive(num-1)        
+
 def solve():
     N, K = list(map(int,input().split()))
-    temp_N = []
-    temp_K = []
-    temp_S = []
-    
-    for i in range(1,N+1):
-        temp_N.append(i)
-        
-    for j in range(1,K+1)
-        temp_K.append(j)
-        
-    for k in range(1,(N-K)+1):
-        temp_S.append(k)
-        
-    print(temp_N)
-    print(temp_K)
-    print(temp_S)    
-    
-    for i in range(0,len(temp_N)):
-        if i == 0:
-            fact_N = temp_N[0]
-        else :
-            fact_N = fact_N*temp_N[i]
+    # N, K = map(int, sys.stdin.readline().strip().split()) # 이렇게도 가능
 
-    for j in range(0,len(temp_K)):
-        if j == 0:
-            fact_K = temp_K[0]
-        else:
-            fact_K = fact_K*temp_K[j]
-            
-    for k in range(0,len(temp_S)):
-        if k == 0:
-            fact_S = temp_S[0]
-        else:
-            fact_S = fact_S*temp_S[k]
-    
-    print(fact_N)
-    print(fact_K)
-    print(fact_S)
-    
-    result = fact_N / (fact_S * fact_K)
-    print(result)
+    result = recursive(N)/(recursive(N-K) * recursive(K))
+    print("{:.0f}".format(result), sep='') # "{:.(소숫점)f}".format(num)
 
 if __name__ == "__main__":
     solve()
