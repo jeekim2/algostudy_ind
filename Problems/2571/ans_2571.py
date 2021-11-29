7de029fa92bdb40cc588b6472325c3f026ebe796
YYH <zombi907@hanmail.net>
2021-11-29 23:04:26 +0900

    daily commit 21.11.29 - YYH

--- Problems/2571/ans_2571.py
+++ Problems/2571/ans_2571.py
@@ -12,20 +12,48 @@ input() 내장 함수는 parameter로 prompt message를 받을 수 있다.
 """
 import sys
 
-#시간 초과
-def solve():
-    N = int(sys.stdin.readline().strip())
-    num_list=[]
-    while len(num_list)< N:        
-        num_list.append(int(sys.stdin.readline().strip()))
+# (1) 순차 정렬(Sequential sort) - 시간 초과
+# - 맨 앞에서부터 뒤의 요소들과 비교하여 작은 것을 발견하면 교환
+# - 앞에서 가장 작은 값을 정렬해가는 방식
+# def solve():
+#     N = int(sys.stdin.readline().strip())
+#     num_list=[]
+#     while len(num_list)< N:        
+#         num_list.append(int(sys.stdin.readline().strip()))
     
-    for i in range(len(num_list)-1):
-        for j in range(i+1, len(num_list)):
-            if num_list[i] > num_list[j]:
-                num_list[i], num_list[j] = num_list[j], num_list[i]
-    for x in num_list : 
-        print(x)
+#     for i in range(len(num_list)-1):
+#         for j in range(i+1, len(num_list)):
+#             if num_list[i] > num_list[j]:
+#                 num_list[i], num_list[j] = num_list[j], num_list[i]
+#     for x in num_list : 
+#         print(x)
 
+# (2) 선택 정렬(Selection sort)
+#  - 첫번째부터 순차적으로 최소값 찾아 교환하기 = 순차 정렬과 같은 개념으로 보임.
+#  - 첫번째에 가장 작은 수를 두고, 두번째에 두번째 작은 수를 둔다. 
+# def solve() :
+#     Arry = [9, 8, 1, 3, 2]
+    
+#     for i in range(len(Arry)-1):
+#         minIndex = i
+#         for j in range(i+1, len(Arry)):
+#             if Arry[j]<Arry[minIndex]:
+#                 minIndex = j
+#         Arry[i], Arry[minIndex] = Arry[minIndex], Arry[i]
+#     print(Arry)
+
+# (3) 버블 정렬(Bubble Sort)
+# - 인접한 두 요소를 비교 및 교환해가면서 최대값 찾기
+# - 가장 큰 수는 마지막에 두고, 두번째 큰 수는 마지막에서 두번째에 두기
+def solve():
+    Arry = [9, 8, 1, 3, 2]
+    
+    for i in range(1, len(Arry)):
+        for j in range(len(Arry)-i):
+            if Arry[j] > Arry[j+1]:
+                Arry[j], Arry[j+1] = Arry[j+1], Arry[j]
+    print(Arry)               
+    
 if __name__ == "__main__":
     solve()
     
