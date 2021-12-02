48a29559c34965293b01e66614db4ae3588bfccc
YYH <zombi907@hanmail.net>
2021-12-02 22:36:23 +0900

    daily commit 21.12.02 - YYH

---
+++ Problems/15649/ans_15649_y1.py
@@ -0,0 +1,12 @@
+#https://www.acmicpc.net/problem/15649
+import sys
+
+def solve():
+    N, M = map(int,sys.stdin.readline().split())
+    # 백트랙킹 공부하고 풀어야겠다.. 어렵다..
+    
+if __name__ == "__main__":
+    solve()
+    
+    
+    
