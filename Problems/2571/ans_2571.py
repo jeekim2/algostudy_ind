2f9b67b480fd33a30183836c07fb79e7e1ef998a
YYH <zombi907@hanmail.net>
2021-12-05 23:21:01 +0900

    daily commit 21.12.05 - YYH

--- Problems/2571/ans_2571.py
+++ Problems/2571/ans_2571.py
@@ -75,37 +75,59 @@ import sys
 # (5) 병합 정렬(Merge Sort)
 #  - 요소 1개까지 온전히 나누고, 다시 단계별로 couple로 묶으면서 정렬
 # 점점 더 어려워지는구만....
-def merge(left, right):
-    result = []
-    while len(left)>0 or len(right)>0:
-        if len(left)>0 and len(right)>0:
-            if left[0]<=right[0]:
-                result.append(left[0])
-                left = left[1:]
-            else:
-                result.append(right[0])
-                right = right[1:]
-        elif len(left)>0:
-            result.append(left[0])
-            left = left[1:]
-        elif len(right)>0:
-            result.append(right[0])
-            right = right[1:]
-        else:
-            pass
-    return result
+# def merge(left, right):
+#     result = []
+#     while len(left)>0 or len(right)>0:
+#         if len(left)>0 and len(right)>0:
+#             if left[0]<=right[0]:
+#                 result.append(left[0])
+#                 left = left[1:]
+#             else:
+#                 result.append(right[0])
+#                 right = right[1:]
+#         elif len(left)>0:
+#             result.append(left[0])
+#             left = left[1:]
+#         elif len(right)>0:
+#             result.append(right[0])
+#             right = right[1:]
+#         else:
+#             pass
+#     return result
 
-def solve(Arry):
-    if len(Arry)<=1:
-        return Arry
-    mid = len(Arry)//2
-    leftArea = Arry[:mid]
-    rightArea = Arry[mid:] 
-    leftArea=solve(leftArea)
-    rightArea=solve(rightArea)
+# def solve(Arry):
+#     if len(Arry)<=1:
+#         return Arry
+#     mid = len(Arry)//2
+#     leftArea = Arry[:mid]
+#     rightArea = Arry[mid:] 
+#     leftArea=solve(leftArea)
+#     rightArea=solve(rightArea)
     
-    return merge(leftArea,rightArea)
-       
+#     return merge(leftArea,rightArea)
+# (6) 퀵 정렬(Quick Sort)
+#  - Pivot을 중심으로 왼쪽은 작은 수의 정렬, 오른쪽은 큰 수의 정렬
+def quick_sort(Arry, start, end):
+    if start>=end:
+        return
+    pivot = start
+    left = start+1
+    right = end
+    
+    while (left <= right):
+        while (left <= end and Arry[left]<=Arry[pivot]):
+            left+=1
+        while (start<right and Arry[pivot]<=Arry[right]):
+            right-=1
+        if (left > right):
+            Arry[right], Arry[pivot] = Arry[pivot], Arry[right]
+        else:
+            Arry[left], Arry[right] = Arry[right], Arry[left]
+        
+    quick_sort(Arry, start, right-1)
+    quick_sort(Arry, right+1, end)
+    
 if __name__ == "__main__":
-    print(solve([9, 8, 1, 3, 2]))
-    
+    Arry = [9, 8, 1, 3, 2]
+    quick_sort(Arry,0,len(Arry)-1)
+    print(Arry)
