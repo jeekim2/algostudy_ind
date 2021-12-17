582fd924779cc4f5ce155307b6b892156c8e3c83
hongmyeong-lee <dlghdaud16@naver.com>
2021-12-17 12:17:07 +0900

    주식가격

---
+++ Problems/programmers_stack_4/42584.py
@@ -0,0 +1,41 @@
+#https://programmers.co.kr/learn/courses/30/lessons/42584
+# 내가 제출한 답, 정확성은 맞았으나 효율성에서 실패 함
+def solution(prices):
+    answer = []
+    cnt = 0
+    temp = prices[1:len(prices)]
+    while prices:
+
+        cnt = cnt +1    
+        if len(prices)==1:  #price 마지막 한개 남으면 비교할 대상이 없기때문에 그냥 exit
+            answer.append(0)
+            break
+
+        if prices[0] <= temp[0]:                       
+            temp.pop(0)
+        else:
+            answer.append(cnt)
+            cnt = 0      
+            prices.pop(0)
+            temp = prices[1:len(prices)]
+        
+        if len(temp)==0:
+            answer.append(cnt)
+            cnt = 0
+            prices.pop(0)
+            temp = prices[1:len(prices)]     
+
+    return answer
+
+
+#다른 사람 풀이
+
+def solution(prices):
+    result =[0]*len(prices) # 우선 가격에 대해 0으로 모두 채워 넣는다
+    for i in range(len(prices)-1):
+        for j in range(i, len(prices)-1):
+            if prices[i] <= prices [j]:
+                result[i] = result[i]+1
+            else:
+                break
+    return result
