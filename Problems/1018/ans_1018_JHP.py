7e4dba970df8250aa0cc58e4f61384be12f2c074
Bobzzang <jaehyun_22@naver.com>
2021-12-19 20:28:48 +0900

    Daily Commit on 2021.12.19 : Resolve Problem 1018

--- Problems/1018/ans_1018_JHP.py
+++ Problems/1018/ans_1018_JHP.py
@@ -11,7 +11,7 @@ def solve():
     
     for _ in range(N):
         board.append(input())
-
+    
     # 브루트포스 풀기
     for i in range(N-7): # 8x8로 자르기 위해 i-7, j-7만큼 고정 필요 (끝에서 7번째)
         for j in range(M-7): # 상동 이유
@@ -19,18 +19,18 @@ def solve():
             first_B = 0
             for k in range(i,i+8): # k~k+8까지 색칠하는 횟수 카운트
                 for v in range(j,j+8):
-                    if (k+v)%2 == 0: # 한칸씩 띄어가며 확인
-                        if board[k][v] != 'W':
+                    if (k+v)%2 == 0: # 짝수 칸에 대한 처리 - 한칸씩 띄어가며 확인
+                        if board[k][v] != 'W': # 짝수 칸이 W라고 가정한 경우
                             first_W += 1
-                        else:
-                            first_B += 1
-                    else:
-                        if board[k][v] != 'B':
+                        else: # 짝수 칸이 B라고 가정한 경우
+                            first_B += 1 
+                    else: # 홀수 칸에 대한 처리 (위에 조건과 반대로 색칠 필요)
+                        if board[k][v] != 'B': # 홀수 칸이 B라고 가정한 경우
                             first_W += 1
-                        else:
-                            first_B += 1
-        result.append(first_B)
-        result.append(first_W)
+                        else: # 홀수 칸이 W라고 가정한 경우
+                            first_B += 1      
+            result.append(first_B)
+            result.append(first_W)
     print(min(result))
         
 if __name__ == '__main__':
