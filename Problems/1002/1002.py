0c3157ffafcf3bed9ba57c54fc1a666225302eb1
hongmyeong-lee <dlghdaud16@naver.com>
2021-12-21 22:22:44 +0900

    1002 fix

--- Problems/1002/1002.py
+++ Problems/1002/1002.py
@@ -15,19 +15,17 @@ for i in range(T):
     #a = input()
     #b = a.split()
     x1, y1, r1, x2, y2, r2 = map(int, input().split())
-    r = ((x1-y1)**2 + (x2-y2)**2)**0.5
-    R = [r1,r2,r]
-    M = max(R)
-    R.remove(M)
-    if r == 0:
-        if r1 == r2:
-            print(-1)
-        else:
-            print(0)
+    r = ((x2-x1)**2 + (y2-y1)**2)**0.5
+
+    if r == 0 and r1 == r2:
+        print(-1)
+    elif r == r1+r2:
+        print(1)
+    elif r == abs(r1-r2):
+        print(1)
+    elif r > r1 +r2:
+        print(0)
+    elif r < abs(r1-r2):
+        print(0)
     else:
-        if M == sum(R):
-            print(1)
-        elif M > sum(R):
-            print(0)
-        else:
-            print(2)
+        print(2)
