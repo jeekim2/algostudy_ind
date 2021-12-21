b27d40d567d71a918172d952b3a7b44e7bca8f4a
hongmyeong-lee <dlghdaud16@naver.com>
2021-12-21 22:02:44 +0900

    problem error fix

--- Problems/1110/1110.py
+++ Problems/1110/1110.py
@@ -1,4 +1,5 @@
 #https://www.acmicpc.net/problem/1110
+'''
 import math
 inputVal = int(input())     #26
 #in1 = math.floor(inputVal/10)
@@ -23,4 +24,16 @@ while True:
     else:
         print(len(input)-2)  
         break
-     
+'''     
+a = int(input())
+b  = -1
+count = 0
+
+while b!=a:
+    if b == -1:
+        b = a
+    b = (b//10 + b%10)%10 + (b%10)*10
+    count = count +1
+
+print(count)
+
--- Problems/programmers_hash_1/42576.py
+++ Problems/programmers_hash_1/42576.py
@@ -1,22 +1,14 @@
 #https://programmers.co.kr/learn/courses/30/lessons/42576?language=python3
 
 def solution(participant, completion):
+    answer = 0 
     participant.sort()
     completion.sort()
     for i in range(len(completion)):
         if participant[i] != completion[i]:
             return participant[i]
-        if i == len(completion) - 1:
+            
+        if i == len(completion)-1:
             return participant[-1]
+            
 
-#문제를 잘못 이해함, 1명만 차이나는 것을 못봄 
-#좋은 답변
-'''
-import collections
-
-def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    ans = collections.counter(participant)-collections.counter(completion)
-    result list(result)[0]
-'''
