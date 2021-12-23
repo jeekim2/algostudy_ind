66341430f3d3bc2b9cd89f9bb883d63ed0807a0b
Bitnuri <qlcsnfl1005@naver.com>
2021-12-23 18:59:02 +0900

    2581 해결

--- Problems/2581/ans_2581_BNR.py
+++ Problems/2581/ans_2581_BNR.py
@@ -8,27 +8,22 @@ N = int(input()) #N 이하
 sum = 0
 min = 0
 
-if N <= 2:
-    if N == 2:
-        sum = 2
+for i in range(N,M-1,-1):
+    n = 2
+    if i == 1:
+        sum = sum
+        min = min
+    elif i == 2:
+        sum = sum + 2
         min = 2
-
-else:
-    for i in range(N,M-1,-1):
-        n = 2
-        if i == 1:
-            cnt = cnt
-        elif i == 2:
-            cnt = cnt + 1
-        else:
-            while n < i:
-                if i % n == 0:
-                    # n = n +1
-                    break
-                if n == i - 1:
-                    sum = sum +i
-                    min = i
-                n = n + 1
+    else:
+        while n < i:
+            if i % n == 0:
+                break
+            if n == i - 1:
+                sum = sum +i
+                min = i
+            n = n + 1
 
 if sum==0 and min == 0:
     print('-1')
