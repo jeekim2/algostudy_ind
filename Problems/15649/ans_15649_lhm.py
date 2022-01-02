a86a5dd2bc655123a718f85d96c779f5f87b55ea
hongmyeong-lee <dlghdaud16@naver.com>
2022-01-02 22:57:48 +0900

    15649 back tracking fix

--- Problems/15649/ans_15649_lhm.py
+++ Problems/15649/ans_15649_lhm.py
@@ -1,4 +1,5 @@
 #https://www.acmicpc.net/problem/15649
+'''
 from itertools import permutations
 x,y = map(int,input().split())
 
@@ -11,3 +12,21 @@ for i in range(len(b)):
     for j in range(len(b[i])):
         print(b[i][j], end=" ")
         #print(" ")
+'''
+n, m = map(int, input().split())    #ex)n=3, m=1
+
+arr = [] #stack 개념, 길이 맞으면 출력
+
+def dfs():
+    if len(arr) == m:
+        print(' '.join(map(str, arr)))
+        return
+    
+    for i in range(1, n+1):
+        if i in arr:
+            continue
+        arr.append(i)
+        dfs()#재귀
+        arr.pop()
+
+dfs()
