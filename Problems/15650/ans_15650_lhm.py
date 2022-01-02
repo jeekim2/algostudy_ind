0759777b40a89cfa50a37279f2e28aaf66f803a1
hongmyeong-lee <dlghdaud16@naver.com>
2022-01-02 23:40:33 +0900

    15650 error fix

--- Problems/15650/ans_15650_lhm.py
+++ Problems/15650/ans_15650_lhm.py
@@ -14,6 +14,8 @@ for i in range(len(temp2)):
 '''
 #주석을 빼야 출력 형식에 맞는거 같은데 오히려 주석처리를 해야 맞네요..
 #15649와 상당히 유사한 문제인데, dfs 함수 안에 인덱스를 넣어서 겹치지 않게 해주면 될 듯 합니다
+import sys
+input= sys.stdin.readline
 n, m = map(int, input().split())    #ex)n=3, m=1
 
 arr = [] #stack 개념, 길이 맞으면 출력
@@ -25,6 +27,8 @@ def dfs(x):
 
     for i in range(x, n+1):
         if i in arr:
+            continue
+        if (len(arr)>=1 and i<arr[-1]):
             continue
         arr.append(i)
         dfs(x+1)#재귀
