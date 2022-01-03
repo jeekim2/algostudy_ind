7c08a8807441bbaf8d138a15da89aa53b208c180
hongmyeong-lee <dlghdaud16@naver.com>
2022-01-03 19:48:50 +0900

    11650 시간초과

--- Problems/11650/11650_lhm.py
+++ Problems/11650/11650_lhm.py
@@ -3,15 +3,24 @@ import sys
 input = sys.stdin.readline
 T = int(input())
 temp = []
+ans = []
 
 
 for i in range(T):
     [x,y] = map(int,input().split())
     temp.append([x,y])
 
-temp.sort()
 
 
-for i in range(len(temp)):
-    print(temp[i][0], temp[i][1])
-   
+
+#temp.sort()
+
+while temp:
+    ans.append(min(temp))
+    temp.pop(temp.index(min(temp)))
+##위에 whlie temp 부분이 sort 대신쓰려고 넣은건데 계속 시간 초과 가 뜨네.... 왜그런지 아시는분
+
+
+
+for i in range(len(ans)):
+    print(ans[i][0], ans[i][1])
