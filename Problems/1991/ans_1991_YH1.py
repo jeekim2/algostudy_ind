c9e6cb364b52b8bb87efc1d0f8cf4bcdd2797a15
YYH <zombi907@hanmail.net>
2022-01-06 22:55:17 +0900

    daily commit 22.01.06

--- Problems/1991/ans_1991_YH1.py
+++ Problems/1991/ans_1991_YH1.py
@@ -1,6 +1,7 @@
 # https://www.acmicpc.net/problem/1991
 # 2022-01-04 : 오늘 야근으로 인한 draft commit
 # 2022-01-05 : 발전이 없네...후... 재귀 input과 return 값을 못 구하겠군..
+# 2022-01-06 : 입력부터 난제... 문자 중복 제외하는 것도 난제...
 import sys
 
 def solve():
@@ -12,15 +13,25 @@ def solve():
     nodes = []
     result = []
     for _ in range(N):
-        a,b,c = input().split()
-        nodes.insert(a,b,c) # root : 1st index, left : 2*index + 1, right : 2*index + 2
-    binary_tree()
+        a,b,c = map(str,input().split())
+        nodes.append(a) # root : 1st index, left : 2*index + 1, right : 2*index + 2
+        nodes.append(b)
+        nodes.append(c)
+    binary_tree(0)
+    print(result)
 
 def binary_tree(idx):
-    result.append(root)
-    for x in nodes:
-        if x[0] == left:
-           return binary_tree(x, mode)
+    left, right = 2*idx+1, 2*idx+2
+    if len(result) == N : return
+    #전위순회
+    result.append(nodes[idx])
+    if left <= N and nodes[left] != '.': return binary_tree(left)
+    if right <= N and nodes[right] != '.': return binary_tree(right)
+
+if __name__ == "__main__":
+    solve()
+
+    
 
 
 
