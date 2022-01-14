02e5de2b414187a9078ef0a0594bd2f58e9b04e0
YYH <zombi907@hanmail.net>
2022-01-14 13:59:34 +0900

    daily commit 22.01.14

--- Problems/7568/ans_7568_YH.py
+++ Problems/7568/ans_7568_YH.py
@@ -1,2 +1,20 @@
 #https://www.acmicpc.net/problem/7568
-#..TT
+#반례가 뭐냐 이거...
+
+import sys
+def solve():
+    input = sys.stdin.readline
+    N = int(input())
+    person, Ratings = [], []
+    for _ in range(N):
+        person.append(list(input().split()))
+
+    for p1 in person:
+        rating = 1
+        for p2 in person:
+            if p1 != p2 :
+                if p1[0] < p2[0] and p1[1] < p2[1]:
+                    rating+=1
+        print(rating, end=" ")
+if __name__=="__main__":
+    solve()
