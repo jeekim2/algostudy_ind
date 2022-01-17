6333c198d4297dfe82f687bbe203e370059628bf
hongmyeong-lee <dlghdaud16@naver.com>
2022-01-17 20:54:55 +0900

    quick sort수정

--- Problems/11399/ans_11399_lhm.py
+++ Problems/11399/ans_11399_lhm.py
@@ -4,26 +4,33 @@ import sys
 input = sys.stdin.readline
 
 def quick_sort(a):  #퀵소트
-    n = len(a)
-    if n <=1:
-        return a
     
-    pivot = a[-1] #pivot 값 임시 지정
+    if len(a) <=1:
+        return a
+
+    pivot = a[len(a)//2] #pivot 값 임시 지정
     s1 = []
     s2 = []
-
-    for i in range(0, n-1):
-        if a[i]<pivot:
-            s1.append(a[i])
+    s =[]
+    for i in a:
+        if i<pivot:
+            s1.append(i)
+        elif i>pivot:
+            s2.append(i)
         else:
-            s2.append(a[i])
-
-    return quick_sort(s1)+[pivot]+quick_sort(s2)
+            s.append(i)
+    #print("a:",a)
+    #print("pivot:",pivot)
+    #print("s1:",s1)
+    #print("s2:",s2)
+    #print("s:",s)
+    return quick_sort(s1)+s+quick_sort(s2)
 
 T = int(input())
 b = list(map(int,input().split()))
-b.sort()
-#b = quick_sort(b) # 퀵소트 쓰면 런타임 에러 뜨네..
+#b.sort()
+b = quick_sort(b)
+
 
 for i in range(1,len(b)):
     b[i] = b[i-1]+b[i]
