7c12643b696c11f852b40ae198676b4ecaaf5da3
Bobzzang <jaehyun_22@naver.com>
2022-01-24 20:46:37 +0900

    Fix the comment - problem number was wrong in previous commit

--- Problems/11047/ans_11047_JHP.py
+++ Problems/11047/ans_11047_JHP.py
@@ -1,4 +1,4 @@
-# https://www.acmicpc.net/problem/13305
+# https://www.acmicpc.net/problem/11047
 # 문제 : 준규가 가지고 있는 동전 N 종류 동전, 동전의 합 = K 가치를 만들 때 최소 동전 갯수
 # 입력 : 첫째 줄 = N, K 둘째 줄 = N개의 줄에 동전의 가치 (오름차순)
 # 출력 : 첫째 줄에 K원을 만드는데 필요한 최소 동전 개수
