3941daa744bdba90c809df8048fa7444559a7d60
YYH <zombi907@hanmail.net>
2022-01-31 01:38:06 +0900

    daily commit 22.01.31

--- Problems/10844/ans_10844_YH.py
+++ Problems/10844/ans_10844_YH.py
@@ -1,2 +1,10 @@
 #https://www.acmicpc.net/problem/10844
 
+def solve():
+    N = int(input())
+    #dp[i] = 오름, 중간, 내림
+    #dp[i] = [N-i, dp[i-1][1]*2 ,N-i+1]
+    #[9,0,0], [8,0,9], [7*2,1,8*2],[6*3,48,7*3], [5,,6] 
+
+    for i in range(N):
+        
