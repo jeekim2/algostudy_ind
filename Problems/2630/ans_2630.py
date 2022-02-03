613727d04c4d3e221f42c00fb0e33c4e5c0a73f1
hongmyeong-lee <dlghdaud16@naver.com>
2022-02-03 09:48:20 +0900

    2630 lhm fix

--- Problems/2630/ans_2630.py
+++ Problems/2630/ans_2630.py
@@ -1,3 +1,5 @@
+#https://www.acmicpc.net/problem/2630
+
 import sys
 input = sys.stdin.readline
 N = int(input())
@@ -6,8 +8,10 @@ result = []
 for i in range(N):
     paper.append(list(map(int, input().split()))) #int 자료형
     
-
+zero = 0
+one = 0
 def solution(x, y, N):
+  global zero, one
   num = paper[x][y]   # 변수에 각 정사각형 종이의 자리 할당 
   for i in range(x, x+N):  # 시작점과 숫자가 같은지 반복문을 돌면서 확인
     for j in range(y, y+N):
@@ -18,11 +22,11 @@ def solution(x, y, N):
         solution(x+N//2, y+N//2, N//2)
         return
   if num == 0:
-    result.append(0)
+    zero = zero + 1
   else: 
-    result.append(1)
+    one = one +1
 
 
 solution(0,0,N)
-print(result.count(0))  # 흰종이
-print(result.count(1))  # 파랑종이
+print(zero)  # 흰종이
+print(one)  # 파랑종이
