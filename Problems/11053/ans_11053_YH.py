93fb04a90b59b570e08968525687cd971cd05040
YYH <zombi907@hanmail.net>
2022-02-05 05:12:01 +0900

    daily commit 22.02.05

--- Problems/11053/ans_11053_YH.py
+++ Problems/11053/ans_11053_YH.py
@@ -1,34 +1,41 @@
 #https://www.acmicpc.net/problem/11053
 
-def solve0(): #FAIL
+# Dynamic Programming   
+def solve1(): # Pass
     N = int(input())
     values = list(map(int, input().split()))
-    dp=[]
-    LIS = 0
-    for i in range(N-1):
-        dp.append(values[i])
-        for j in range(i,N):
-            if values[j] > dp[-1]:
-                dp.append(values[j])
-            elif values[j] > dp[j-2]:
-                dp[j-2] = values[j]
-            else:
-                pass            
-        LIS = max(LIS, len(dp))
-        dp=[]
-    print(LIS)
-    
-def solve1():
-    N = int(input())
-    values = list(map(int, input().split()))
-    dp=[1]*(N)
+    dp=[1]*(N) # dp[i] : i번째 index에서의 최장 증가 부분 수열(LIS)
     
     for i in range(1,N):
         for j in range(i):
             if values[i] > values[j]:
                 dp[i] = max(dp[i],dp[j]+1)
     print(max(dp))
-                
+    
+# 완전 탐색
+def solve2(): # 정답으로 보이나 O(2^N)으로 시간 초과
+    global N, values
+    N = int(input())
+    values = list(map(int, input().split()))
+    print(LIS(values))
+    
+def LIS(arry):
+    if not arry: return 0
+    
+    ret = 1 # ret : return값
+    for i in range(len(arry)): # arry[i] : LIS의 첫 수
+        next = [] # LIS를 이루는 arry[i] 뒤에 이어질 후보값들
+        for j in range(i,len(arry)):
+            if arry[i] < arry[j]:
+                next.append(arry[j]) 
+        ret = max(ret, LIS(next)+1)
+    return ret
 
-if __name__=="__main__":
-    solve1()
+# 이진탐색
+def solve3():
+
+
+if __name__== "__main__":
+    solve1() #Pass
+    solve2() #시간초과
+    solve3() #
