b8bca31d9abbb3f029276423e21c4d553800540e
YYH <zombi907@hanmail.net>
2022-02-06 04:33:06 +0900

    daily commit 22.02.06

--- Problems/11053/ans_11053_YH.py
+++ Problems/11053/ans_11053_YH.py
@@ -4,7 +4,7 @@
 def solve1(): # Pass
     N = int(input())
     values = list(map(int, input().split()))
-    dp=[1]*(N) # dp[i] : i번째 index에서의 최장 증가 부분 수열(LIS)
+    dp=[1]*(N) # dp[i] : i번째 index까지 배열 중 최장 증가 부분 수열(LIS)
     
     for i in range(1,N):
         for j in range(i):
@@ -33,9 +33,33 @@ def LIS(arry):
 
 # 이진탐색
 def solve3():
+    global LIS
+    N = int(input())
+    values = list(map(int, input().split()))
+    LIS = [] #최장 증가 부분 수열
+    LIS.append(values[0]) #초기화
+    j=0
+    for i in range(1, N):
+        if values[i] > LIS[j]:
+            LIS.append(values[i])
+            j+=1
+        else:
+            idx = binary_search(0,j,values[i]) # LIS 배열 중 적정 위치를 이진 탐색으로 찾기
+            LIS[idx] = values[i]
+        
+    print(len(LIS))        
+    
+def binary_search(left, right, target):
+    while (left < right):
+        mid = (left+right)//2
+        if LIS[mid] < target:
+            left = mid+1
+        else:
+            right = mid
+    return right
 
 
 if __name__== "__main__":
     solve1() #Pass
     solve2() #시간초과
-    solve3() #
+    solve3() #Pass
