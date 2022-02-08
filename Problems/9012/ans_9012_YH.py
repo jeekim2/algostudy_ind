32f7da572ee1505082862b790bfe494edda633c4
YYH <zombi907@hanmail.net>
2022-02-09 06:08:39 +0900

    daily commit 22.02.09:

--- Problems/9012/ans_9012_YH.py
+++ Problems/9012/ans_9012_YH.py
@@ -1,8 +1,43 @@
 #https://www.acmicpc.net/problem/9012
+# 0209 skip
+import sys
 
-def solve():
-    #
-    
+def solve1(): #잘못된 풀이 - 반례 : 1, )(
+    input = sys.stdin.readline
+    T = int(input())
+    cnt = 0
+    result = []
+    for _ in range(T):
+        PS = list(input().rstrip())
+        while len(PS)>0:
+            temp = PS.pop()
+            if  temp == '(':
+                cnt+=1
+            else:
+                cnt-=1
+        if abs(cnt) : result.append("NO")
+        else: result.append("YES")
+    print("\n".join(result))
+
+def solve2():
+    input = sys.stdin.readline
+    T = int(input())
+    stack = []
+    n=0
+    for _ in range(T):
+        PS = list(input().rstrip())
+        for x in PS:
+            if x == '(':
+                stack.append(x)
+                n+=1
+            else:
+                if len(stack) >0:
+                    stack.pop()
+                    n-=1
+                else:
+                    n+=1
+        if len(stack) == 0 and n == 0 : print("YES")
+        else : print("NO")
 
 if __name__ == "__main__":
-    solve()
+    solve2()
