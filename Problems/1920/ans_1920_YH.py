51bf06b57192fe82110bfa159ee87f8bbe6dbdf5
YYH <zombi907@hanmail.net>
2022-02-15 05:10:27 +0900

    daily commit 22.02.15

--- Problems/1920/ans_1920_YH.py
+++ Problems/1920/ans_1920_YH.py
@@ -1,2 +1,8 @@
 #https://www.acmicpc.net/problem/1920
 ###
+def solve():
+    N = int(input())
+    A_arry = [int(input()) for _ in range(N)]
+    M = int(input())
+    M_arry = [int(input()) for _ in range(M)]
+    #
