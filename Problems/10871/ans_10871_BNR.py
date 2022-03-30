e5fc0ca85ced3ed4aa44ebcd4dd7920ab6237054
BitnuriKim <qlcsnfl1005@naver.com>
2022-03-30 19:15:21 +0900

    220330_DailyCommit_10871_BNR

---
+++ Problems/10871/ans_10871_BNR.py
@@ -0,0 +1,21 @@
+'''
+https://www.acmicpc.net/problem/12852
+
+
+'''
+import sys
+
+def findNum(TC, Th):
+    if TC<Th:
+        print(TC, end=' ')
+
+def solve():
+    input = sys.stdin.readline
+    N, Max = map(int, input().split())
+    TC_list = list(map(int,input().split()))
+
+    for i in TC_list:
+        findNum(i, Max)
+    
+if __name__ == "__main__":
+    solve()
