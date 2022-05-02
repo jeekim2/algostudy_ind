b6656c88d2d7c83abd948f07ed419347a2d5488c
YYH <zombi907@hanmail.net>
2022-05-02 21:13:05 +0900

    daily commit 22.05.02

---
+++ Problems/10866/ans_10866_YH.py
@@ -0,0 +1,37 @@
+#https://www.acmicpc.net/problem/10866
+
+from dataclasses import dataclass
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+
+class Deque():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    def Push_front(self):
+        return
+    def Push_back(self):
+        return
+    def Pop_front(self):
+        return
+    def Pop_back(self):
+        return
+    def qsize(self):
+        return
+    def isempty(self):
+        return
+    def qfront(self):
+        return
+    def qback(self):
+        return
+
+def solve():
+    return
+
+if __name__=="__main__":
+    solve()
