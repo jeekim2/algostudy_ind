78cd106f12332d7961b7ab86f8e5544dc911b470
YYH <zombi907@hanmail.net>
2022-05-03 22:31:28 +0900

    daily commit 22.05.03

--- Problems/10866/ans_10866_YH.py
+++ Problems/10866/ans_10866_YH.py
@@ -1,6 +1,5 @@
 #https://www.acmicpc.net/problem/10866
 
-from dataclasses import dataclass
 
 class Node:
     def __init__(self,data):
@@ -8,30 +7,87 @@ class Node:
         self.prev = None
         self.next = None
 
-
-class Deque():
+class Deque:
     def __init__(self):
-        self.head = None
-        self.tail = None
-    def Push_front(self):
-        return
-    def Push_back(self):
-        return
+        self.head = Node(None)
+        self.tail = Node(None)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.size = 0
+    def Push_front(self,data):
+        if self.head == None:
+            self.head = self.tail = Node(data)
+        else:
+            NewNode = Node(data)
+            NewNode.prev = self.head
+            NewNode.next = self.head.next
+            self.head = NewNode
+        self.size +=1
+    def Push_back(self,data):
+        if self.tail == None:
+            self.head = self.tail = Node(data)
+        else:
+            NewNode = Node(data)
+            NewNode.prev = self.tail.prev
+            NewNode.next = self.tail
+            self.tail = NewNode
+        self.size +=1
     def Pop_front(self):
-        return
+        if self.size !=0:
+            temp = self.head.data
+            self.head = self.head.next
+            self.size-=1
+            return temp
+        return -1
     def Pop_back(self):
-        return
+        if self.size !=0:
+            temp = self.tail.data
+            self.tail = self.tail.prev
+            self.size-=1
+            return temp
+        return -1
     def qsize(self):
-        return
-    def isempty(self):
-        return
+        return self.size
+    def is_empty(self):
+        if self.size == 0:
+            return 1
+        return 0
     def qfront(self):
-        return
+        if self.size !=0:
+            return self.head.data
+        return -1
     def qback(self):
-        return
+        if self.size !=0:
+            return self.tail.data
+        return -1
 
 def solve():
-    return
+    N = int(input())
+    deque = Deque()
+    for _ in range(N):
+        cmd = list(input().split())
+        if cmd[0] == "push_front":
+            deque.Push_front(int(cmd[1]))
+        elif cmd[0] == "push_back":
+            deque.Push_back(int(cmd[1]))
+        elif cmd[0] == "front":
+            res = deque.qfront()
+            print(res)
+        elif cmd[0] == "back":
+            res = deque.qback()
+            print(res)
+        elif cmd[0] == "size":
+            res = deque.qsize()
+            print(res)
+        elif cmd[0] == "empty":
+            res = deque.is_empty()
+            print(res)
+        elif cmd[0] == "pop_front":
+            res = deque.Pop_front()
+            print(res)
+        else:
+            res = deque.Pop_back()
+            print(res)
 
-if __name__=="__main__":
+if __name__== "__main__":
     solve()
