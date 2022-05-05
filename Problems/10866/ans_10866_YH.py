818dc1b4564ebed815690e9e0e2c58d46eaef41a
YYH <zombi907@hanmail.net>
2022-05-05 14:34:32 +0900

    daily commit 22.05.05

--- Problems/10866/ans_10866_YH.py
+++ Problems/10866/ans_10866_YH.py
@@ -1,5 +1,6 @@
 #https://www.acmicpc.net/problem/10866
-
+import sys
+input = sys.stdin.readline
 
 class Node:
     def __init__(self,data):
@@ -9,57 +10,59 @@ class Node:
 
 class Deque:
     def __init__(self):
-        self.head = Node(None)
-        self.tail = Node(None)
-        self.head.next = self.tail
-        self.tail.prev = self.head
         self.size = 0
+        self.head = None
+        self.tail = None
     def Push_front(self,data):
+        self.size +=1
+        NewNode = Node(data)
         if self.head == None:
-            self.head = self.tail = Node(data)
-        else:
-            NewNode = Node(data)
-            NewNode.prev = self.head
-            NewNode.next = self.head.next
-            self.head = NewNode
-        self.size +=1
+            self.head = self.tail = NewNode
+            return 
+        # NewNode.prev = self.head
+        # NewNode.next = self.head.next
+        self.head.prev = NewNode
+        NewNode.next = self.head
+        self.head = NewNode
     def Push_back(self,data):
-        if self.tail == None:
-            self.head = self.tail = Node(data)
-        else:
-            NewNode = Node(data)
-            NewNode.prev = self.tail.prev
-            NewNode.next = self.tail
-            self.tail = NewNode
         self.size +=1
+        NewNode = Node(data)
+        if self.head == None:
+            self.head = self.tail = NewNode
+            return  
+        # NewNode.prev = self.tail.prev
+        # NewNode.next = self.tail
+        self.tail.next = NewNode
+        NewNode.prev = self.tail
+        self.tail = NewNode
     def Pop_front(self):
-        if self.size !=0:
-            temp = self.head.data
-            self.head = self.head.next
-            self.size-=1
-            return temp
-        return -1
+        if not self.head:
+            return -1
+        self.size-=1
+        temp = self.head.data
+        self.head = self.head.next
+        if not self.head : 
+            self.tail = None
+        else: 
+            self.head.prev = None
+        return temp
     def Pop_back(self):
-        if self.size !=0:
-            temp = self.tail.data
-            self.tail = self.tail.prev
-            self.size-=1
-            return temp
-        return -1
-    def qsize(self):
-        return self.size
+        if not self.head:
+            return -1
+        self.size-=1
+        temp = self.tail.data
+        self.tail = self.tail.prev
+        if not self.tail : 
+            self.head = None
+        else:
+            self.tail.next = None
+        return temp
     def is_empty(self):
-        if self.size == 0:
-            return 1
-        return 0
+        return +(not self.head)
     def qfront(self):
-        if self.size !=0:
-            return self.head.data
-        return -1
+        return self.head.data if self.head else -1
     def qback(self):
-        if self.size !=0:
-            return self.tail.data
-        return -1
+        return self.tail.data if self.tail else -1
 
 def solve():
     N = int(input())
@@ -67,27 +70,21 @@ def solve():
     for _ in range(N):
         cmd = list(input().split())
         if cmd[0] == "push_front":
-            deque.Push_front(int(cmd[1]))
+            deque.Push_front(cmd[1])
         elif cmd[0] == "push_back":
-            deque.Push_back(int(cmd[1]))
+            deque.Push_back(cmd[1])
         elif cmd[0] == "front":
-            res = deque.qfront()
-            print(res)
+            print(deque.qfront())
         elif cmd[0] == "back":
-            res = deque.qback()
-            print(res)
-        elif cmd[0] == "size":
-            res = deque.qsize()
-            print(res)
+            print(deque.qback())
         elif cmd[0] == "empty":
-            res = deque.is_empty()
-            print(res)
+            print(deque.is_empty())
         elif cmd[0] == "pop_front":
-            res = deque.Pop_front()
-            print(res)
+            print(deque.Pop_front())
+        elif cmd[0] == "pop_back":
+            print(deque.Pop_back())
         else:
-            res = deque.Pop_back()
-            print(res)
+            print(deque.size)
 
 if __name__== "__main__":
     solve()
