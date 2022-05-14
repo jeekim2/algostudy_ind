ef9a1031490312142ae8926c2e393ddfba0f8b9c
jeekim2 <jeekim2@gmail.com>
2022-05-14 10:45:43 +0900

    Daily commit 220514 - Problem 10166 - Optimized

--- Problems/10166/ans_10166_JHK.py
+++ Problems/10166/ans_10166_JHK.py
@@ -3,16 +3,25 @@
 
 def solve():
     D1, D2 = map(int, input().split())
-    res = set()
-    res.add((0, 0))
-    for v in range(D1, D2 + 1):
-        for a in range(1, v):
+    res = {}
+    cnt = 0
+    if D1 > D2 // 2:
+        start = D1
+    else:
+        start = D2 // 2
+    for v in range(start, D2 + 1):
+        temp = []
+        for a in range(1, v + 1):
             p = a
             b = v
             while b % a != 0:
                 a, b = b % a, a
-            res.add((v // a, p // a))
-    print(len(res))
+            if v // a not in res:
+                temp.append(v // a)
+                cnt += 1
+        for t in temp:
+            res[t] = 1
+    print(cnt)
     return
 
 
