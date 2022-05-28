740d36e64fc47581111ee6af75cf68605a1a1c6e
Bobzzang <jaehyun_22@naver.com>
2022-05-28 22:58:17 +0900

    Daily Commit 2022.05.28 : Problem 2164 Card2 modified

--- Problems/2164/ans_2164_JHP.py
+++ Problems/2164/ans_2164_JHP.py
@@ -2,8 +2,30 @@
 # 문제 : N개의 카드 중 마지막에 남는 카드의 숫자 찾기
 # 입력 : N개의 카드 숫자
 # 출력 : 마지막에 남는 카드 찾기
+# --------------------------------------------------------------
+# deque(double-end queue)를 사용 해야 시간초과 없이 해결 가능
+# deque는 append와 pop에 대해 O(1) 시간 복잡도를 가지고 있음
+# List의 append와 pop은 O(n) 시간 복잡도를 지니기 때문에, deque가 보다 효과적임
+# https://blog.naver.com/dsz08082/222559458327
 
 import sys
+from collections import deque
+
+def solve():
+    input = sys.stdin.readline
+    N = int(input())
+    queue = deque()
+
+    for i in range(1, N+1):
+        queue.append(i)
+
+    while len(queue)>1:
+        queue.popleft()
+        queue.append(queue.popleft())
+    print(queue[-1])
+
+if __name__ == '__main__':
+    solve()
 
 ''' 시간 초과 
 def solve_failed():
@@ -21,20 +43,4 @@ def solve_failed():
         queue.remove(queue[0])
     print(queue[-1])
 '''
-from collections import deque
 
-def solve():
-    input = sys.stdin.readline
-    N = int(input())
-    queue = deque()
-
-    for i in range(1, N + 1):
-        queue.append(i)
-
-    while len(queue) > 1:
-        queue.popleft()
-        queue.append(queue.popleft())
-    print(queue[-1])
-
-if __name__ == '__main__':
-    solve()
