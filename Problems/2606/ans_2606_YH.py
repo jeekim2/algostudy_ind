deb92e601b689a21277b87ff7e4ca5fdc8cd1412
YYH <zombi907@hanmail.net>
2022-07-05 22:40:52 +0900

    daily commit 22.07.05

--- Problems/2606/ans_2606_YH.py
+++ Problems/2606/ans_2606_YH.py
@@ -1,6 +1,7 @@
 #https://www.acmicpc.net/problem/2606
 from collections import deque
 
+# bfs로 풀기
 def bfs(node, graph, visited):
     queue = deque([node])
     visited[node] = True
@@ -15,6 +16,22 @@ def bfs(node, graph, visited):
                 cnt+=1
     return cnt
 
+# dfs로 풀기
+def dfs(node, graph, visited):
+    stack = deque([node])
+    visited[node] = True
+    cnt = 0
+
+    while stack:
+        v = stack.pop()
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = True
+                stack.append(i)
+                cnt+=1
+                print(i, end=" ")
+    return cnt
+
 def solve():
     n = int(input())
     m = int(input())
@@ -25,13 +42,14 @@ def solve():
             graph[a] = [b]
         else:
             graph[a].append(b)
-        if b not in graph:
+        if b not in graph: # For bidirection
             graph[b] = [a]
         else:
             graph[b].append(a)
-
+    print(graph)
     visited = [False]*(n+1)
-    print(bfs(1, graph, visited))
+    # print(bfs(1, graph, visited))
+    print(dfs(1, graph, visited))
 
     return
 
