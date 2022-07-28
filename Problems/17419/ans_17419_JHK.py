b482e5e636715728602f71327fefd91ffddcf39f
Jihwan Kim <jeekim2@Jihwans-MacBook-Air.local>
2022-07-28 18:24:02 +0900

    Daily commit 220728 - Problem 17419

---
+++ Problems/17419/ans_17419_JHK.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/17419
+
+
+def solve():
+    N = int(input())
+    NumStr = input()
+    print(NumStr.count("1"))
+    return
+
+
+if __name__ == "__main__":
+    solve()
