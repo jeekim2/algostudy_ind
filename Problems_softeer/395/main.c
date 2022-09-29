8276a252e2a7ce3148073fb9c5cfebac0ff19f88
jeekim2 <jeekim2@gmail.com>
2022-09-29 23:02:15 +0900

    Daily commit 220929 - Problem Softeer 395

---
+++ Problems_softeer/395/main.c
@@ -0,0 +1,36 @@
+// https://softeer.ai/practice/info.do?idx=1&eid=395
+
+#include <stdio.h>
+
+int main(void) {
+    int W, N, M, P;
+    int i;
+    int JewelWeight[10001] = {
+        0,
+    };
+    int res;
+    scanf("%d%d", &W, &N);
+    i = 0;
+    while (i < N) {
+        scanf("%d%d", &M, &P);
+        JewelWeight[P] = JewelWeight[P] + M;
+        i++;
+    }
+    i = 10000;
+    res = 0;
+    while (W > 0) {
+        if (JewelWeight[i] != 0) {
+            if (W >= JewelWeight[i]) {
+                res += i * JewelWeight[i];
+                W -= JewelWeight[i];
+            } else {
+                res += i * W;
+                W = 0;
+            }
+        }
+        i--;
+    }
+    printf("%d", res);
+
+    return 0;
+}
