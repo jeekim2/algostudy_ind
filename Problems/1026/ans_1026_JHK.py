9f28dadc837cdd17ef9beeae1320f016ba42943b
Jihwan Kim <jeekim2@Jihwans-MacBook-Air.local>
2022-11-08 22:40:04 +0900

    Daily commit 221108 - Problem 1026

---
+++ Problems/1026/ans_1026_JHK.py
@@ -0,0 +1,42 @@
+# https://www.acmicpc.net/problem/1026
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    def merge(left, right):
+        i = 0
+        j = 0
+        res = []
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                res.append(left[i])
+                i += 1
+            else:
+                res.append(right[j])
+                j += 1
+        while i < len(left):
+            res.append(left[i])
+            i += 1
+        while j < len(right):
+            res.append(right[j])
+            j += 1
+        return res
+
+    return merge(merge_sort(arr[: len(arr) // 2]), merge_sort(arr[len(arr) // 2 :]))
+
+
+def solve():
+    N = int(input())
+    A = merge_sort(list(map(int, input().split())))
+    B = merge_sort(list(map(int, input().split())))
+    res = 0
+    for i in range(N):
+        res += A[i] * B[N - i - 1]
+    print(res)
+    return
+
+
+if __name__ == "__main__":
+    solve()
